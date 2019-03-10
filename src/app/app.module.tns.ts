331a3a5dba935089efe05474734e7b4bbe236064
Reece Boyd <rboyd36@gatech.edu>
2019-03-10 14:48:26 -0500

    Added initial project skeleton

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -4,6 +4,15 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AssignmentComponent } from './components/assignment/assignment.component';
+import { AssignmentListComponent } from './components/assignment-list/assignment-list.component';
+import { AssignmentDetailComponent } from './components/assignment-detail/assignment-detail.component';
+import { StudentComponent } from './components/student/student.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { AttemptListComponent } from './components/attempt-list/attempt-list.component';
+import { AttemptComponent } from './components/attempt/attempt.component';
+import { LoginComponent } from './components/login/login.component';
+import { LanguageComponent } from './components/language/language.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
@@ -16,6 +25,15 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    AssignmentComponent,
+    AssignmentListComponent,
+    AssignmentDetailComponent,
+    StudentComponent,
+    StudentListComponent,
+    AttemptListComponent,
+    AttemptComponent,
+    LoginComponent,
+    LanguageComponent,
   ],
   imports: [
     NativeScriptModule,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AssignmentComponent } from './components/assignment/assignment.component';
+import { AssignmentListComponent } from './components/assignment-list/assignment-list.component';
+import { AssignmentDetailComponent } from './components/assignment-detail/assignment-detail.component';
+import { StudentComponent } from './components/student/student.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { AttemptListComponent } from './components/attempt-list/attempt-list.component';
+import { AttemptComponent } from './components/attempt/attempt.component';
+import { LoginComponent } from './components/login/login.component';
+import { LanguageComponent } from './components/language/language.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AssignmentComponent,
+    AssignmentListComponent,
+    AssignmentDetailComponent,
+    StudentComponent,
+    StudentListComponent,
+    AttemptListComponent,
+    AttemptComponent,
+    LoginComponent,
+    LanguageComponent,
   ],
   imports: [
     BrowserModule,
