07520e1bc75873125a45237ef7c9885af6b32d6c
Reece Boyd <rboyd36@gatech.edu>
2019-03-17 21:04:01 -0500

    Added assignment-control component

---
+++ src/app/components/assignment-control/assignment-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-control',
+  templateUrl: './assignment-control.component.html',
+  styleUrls: ['./assignment-control.component.css']
+})
+export class AssignmentControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
