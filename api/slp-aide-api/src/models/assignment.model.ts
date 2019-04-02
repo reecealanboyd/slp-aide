1597159edc41884cf998d91f90b88894e80f2e44
Reece Boyd <rboyd36@gatech.edu>
2019-04-01 21:57:14 -0500

    basic CRUD API for assignment

--- api/slp-aide-api/src/models/assignment.model.ts
+++ api/slp-aide-api/src/models/assignment.model.ts
@@ -3,15 +3,22 @@ import {Entity, model, property} from '@loopback/repository';
 @model()
 export class Assignment extends Entity {
   @property({
+    type: 'number',
+    id: true,
+    required: true,
+  })
+  id: number;
+
+  @property({
     type: 'string',
     required: true,
   })
   name: string;
 
   @property({
-    type: 'any',
+    type: 'string',
   })
-  image?: any;
+  image?: string;
 
   @property({
     type: 'object',
